fed70c6ccdac5b802511dac475748b9a868cc47e
McArroy <132247939+McArroy@users.noreply.github.com>
2025-05-15 12:33:09 +0700

    Removed unused codes

--- static/styles/js/main.js
+++ static/styles/js/main.js
@@ -32,16 +32,6 @@
         return false;
     });
 
-
-    // Date and time picker
-    $('#date').datetimepicker({
-        format: 'L'
-    });
-    $('#time').datetimepicker({
-        format: 'LT'
-    });
-
-
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
         center: true,
